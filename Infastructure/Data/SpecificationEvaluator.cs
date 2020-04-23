df0637c5842ee649885fcc2ff40ba6dd495536c8
Jignesh Patel <jpatel@Jigneshs-Mac-mini.local>
2020-04-22 22:02:36 -0400

    adding missing files

---
+++ Infastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity 
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+            if(spec.Criteria != null )
+            {
+                query  = query.Where(spec.Criteria); //e.g  p => p.producttype == id
+            }
+          /* return await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .ToListAsync(); */
+            //following line "includes" from above
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
