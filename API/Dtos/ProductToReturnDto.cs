f4623c173e061f2ad46fe3a977ce65c6547afd75
Jignesh Patel <jpatel@Jigneshs-Mac-mini.local>
2020-04-22 21:59:49 -0400

    End of section 4

---
+++ API/Dtos/ProductToReturnDto.cs
@@ -0,0 +1,17 @@
+namespace API.Dtos
+{
+    //data transfer object to return dtos
+    //no businness logic
+    //only setters and getter
+    public class ProductToReturnDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string PictureUrl { get; set; }
+        public string ProductType { get; set; }
+        public string ProductBrand { get; set; }
+
+    }
+}
