a1c30e420437b881aa8082e0dfe90af036a4598e
jpatel07 <jpatel@Jigneshs-Mac-mini.local>
2020-04-30 23:38:24 -0400

    9 - Bulding the UI for the shop

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +8,6 @@ import { IPagination } from './models/pagination';
 export class AppComponent implements OnInit {
 
   title = 'Skinet';
-  constructor(private http: HttpClient) {}
-  products: IProduct[];
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((
-      response: IPagination) => {
-      this.products = response.data;
-    }), error => {
-      console.log(error);
-    }
-  }
+  constructor() {}
+  ngOnInit(): void { }
 }
