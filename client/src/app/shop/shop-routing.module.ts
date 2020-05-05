55b7989a7ce3eb59ea15d32349294ae71b167423
jpatel07 <j3patel@gmail.com>
2020-05-04 22:20:23 -0400

    12 Added Client - Pazzaz - section header breadcrumbs, loading indicator and homepage

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ShopComponent } from './shop.component';
-import { Routes, RouterModule, Router } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  {path: '', component: ShopComponent},
-  {path: ':id', component: ProductDetailsComponent},
+  { path: '', component: ShopComponent },
+  { path: ':id', component: ProductDetailsComponent, data: { breadcrumb: { alias: 'productDetails' } } },
 ]
 
 @NgModule({
