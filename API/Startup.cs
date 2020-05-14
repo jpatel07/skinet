f122e5a876c1c013165fcd32c17b169731351b29
jpatel07 <j3patel@gmail.com>
2020-05-13 21:21:38 -0400

    15 Added API identity and Account Controller

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using API.Helpers;
 using API.Middleware;
 using AutoMapper;
 using Infastructure.Data;
+using Infastructure.Identity;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +29,11 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            
+            services.AddDbContext<AppIdentityDbContext>( x =>
+            {
+                x.UseSqlite(_config.GetConnectionString("IdentityConnection"));
+            });
 
             services.AddSingleton<IConnectionMultiplexer>(c => {
                 var configuration = ConfigurationOptions.Parse(_config
@@ -36,6 +42,7 @@ namespace API
             });
 
             services.AddApplicationServices();
+            services.AddIdentityServices(_config); // identity related
             services.AddSwaggerDocumentation();
             services.AddCors(opt => 
             {
@@ -62,6 +69,8 @@ namespace API
             app.UseStaticFiles();
 
             app.UseCors("CorsPolicy");//before Authorization
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
