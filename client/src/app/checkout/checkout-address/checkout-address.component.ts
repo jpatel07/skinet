b4404367dd58a14ac0736c398b8dc2577e6a552d
jpatel07 <j3patel@gmail.com>
2020-06-15 22:04:52 -0400

    Enable - linear mode on the stepper and disabled step clicking

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { AccountService } from 'src/app/account/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { IAddress } from 'src/app/shared/models/address';
 
 
 @Component({
@@ -18,8 +19,10 @@ export class CheckoutAddressComponent implements OnInit {
   }
 
   saveUserAddress() {
-    this.accountService.updateUserAddress(this.checkoutForm.get('addressForm').value).subscribe(() => {
+    this.accountService.updateUserAddress(this.checkoutForm.get('addressForm').value)
+      .subscribe((address: IAddress) => {
       this.toaster.success('Address saved');
+      this.checkoutForm.get('addressForm').reset(address);
     }, error => {
       this.toaster.error(error.message);
       console.log(error);
